92e67997f3a1c2d8d5ced96f7e2e1383b5a41b2d
William Sedlacek <wsedlacekc@gmail.com>
2019-11-14 17:05:08 -0600

    Setup Server
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,5 +1,10 @@
 import * as express from 'express';
-import { getAll } from '../hobbits/hobbitsModel';
+import {
+  getAll,
+  insert,
+  update,
+  remove
+} from '../data/helpers/hobbits/hobbits';
 
 export const server = express();
 
@@ -9,12 +14,22 @@ server.get('/', (req, res) => {
   res.status(200).json({ api: 'up' });
 });
 
-server.get('/hobbits', (req, res) => {
-  getAll()
-    .then((hobbits) => {
-      res.status(200).json(hobbits);
-    })
-    .catch((error) => {
-      res.status(500).json(error);
-    });
+server.get('/hobbits', async (req, res) => {
+  const hobbits = await getAll();
+  res.json(hobbits);
+});
+
+server.post('/hobbits', async ({ body }, res) => {
+  const hobbit = await insert(body);
+  res.status(201).json(hobbit);
+});
+
+server.put('/hobbits/:id', async ({ body, params: { id } }, res) => {
+  const hobbit = await update(id, body);
+  res.json(hobbit);
+});
+
+server.delete('/hobbits/:id', async ({ params: { id } }, res) => {
+  const hobbit = await remove(id);
+  res.json(hobbit);
 });
